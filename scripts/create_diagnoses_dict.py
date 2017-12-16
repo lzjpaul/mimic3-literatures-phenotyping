6b484e0f969c315a1062b27042da61120e44c408
ubuntu-yk <smileyk11@foxmail.com>
2017-12-16 02:41:04 -0800

    fix error

--- scripts/create_diagnoses_dict.py
+++ scripts/create_diagnoses_dict.py
@@ -7,63 +7,7 @@ import re
 from sklearn.feature_extraction.text import CountVectorizer
 
 from utility.csv_utility import CsvUtility
-
-
-
-
-# extract the dict of ICD9 codes and map it to its descriptions from the standard file DC_3D11.txt
-# with open("../data-repository/DC_3D11.txt", 'r') as f:
-#     rows = f.readlines()
-#     i = 0
-#     for row in rows:
-#         words = row.strip().split("\t")
-#         if len(words) > 1 and '.' not in words[0] and any(char.isdigit() for char in words[0]):
-#             assert words[0] not in diagnoses_dict
-#             wordlist = [re.sub("[^a-zA-Z-]", "", x.lower()) for x in words[1].split(' ')
-#                         if re.sub("[^a-zA-Z-]", "", x.lower()) not in stop_list]
-#             diagnoses_dict[words[0].strip()] = wordlist
-#             for word in wordlist:
-#                 word_count[word] = word_count[word] + 1 if word in word_count else 1
-#         i += 1
-#         sys.stdout.write('\rROW {0} of {1}...'.format(i, len(rows)))
-#     with open("../data-repository/diagnoses_dict.csv", 'w') as w:
-#         for (key, value) in sorted(diagnoses_dict.items(), reverse=True):
-#             w.write(key + "," + " ".join(value) + "\n")
-#     with open("../data-repository/diagnoses_word_dict.csv", 'w') as w:
-#         for (key, value) in sorted(word_count.items(), key=lambda s: s[1], reverse=True):
-#             w.write(key + "," + str(value) + "\n")
-
-# merge the ICD code with the ICD code from the MIMIC data
-# data_diagnoses = pd.read_csv("../data-repository/diagnosis_counts.csv")
-# print 'data diagnoses size : ', data_diagnoses.shape[0]
-# icd_code = [x[:4] if x[0] == 'E' else x[:3] for x in data_diagnoses["ICD9_CODE"]]
-# # print icd_code
-# data_diagnoses["ICD9_CODE"] = icd_code
-# data_diagnoses.set_index(["ICD9_CODE"], inplace=True)
-# diagnoses_dict = pd.read_csv("../data-repository/diagnoses_dict.csv", index_col=[0])
-# print 'diagnoses dict size :', diagnoses_dict.shape[0]
-# diagnoses_dict.index.name = 'ICD'
-# diagnoses_dict.columns = ['DESCRIPTION']
-# merge_diagnoses = diagnoses_dict.merge(data_diagnoses, how='inner', left_index=True, right_index=True)
-# merge_diagnoses.index.name = "ICD9CODE"
-# print 'after the merge, the diagnoses size : ', len(set(list(merge_diagnoses.index)))
-# merge_diagnoses.to_csv("../data-repository/merge_diagnoses.csv")
-
-# here can process the short title and long title from data, but here i use the standard ICD description
-# ......
-# merge_diagnoses["DESCRIPTION"].drop_duplicates().to_csv("../data-repository/merge_diagnoses_dict.csv")
-# count_vec = CountVectorizer()
-# re = count_vec.fit_transform(list(merge_diagnoses["DESCRIPTION"].drop_duplicates()))
-# print len(count_vec.get_feature_names())
-# print re.toarray().sum(axis=0)
-# count = re.toarray().sum(axis=0)
-# wordslist = count_vec.get_feature_names()
-# merge_diagnoses_word_count = {}
-# for i in range(len(wordslist)):
-#     merge_diagnoses_word_count[wordslist[i]] = count[i]
-# with open("../data-repository/merge_diagnoses_word_dict.csv", 'w') as w:
-#     for (key, value) in sorted(merge_diagnoses_word_count.items(), key=lambda s: s[1], reverse=True):
-#         w.write(key + "," + str(value) + "\n")
+from os import path
 
 # ====================================================================================================
 # 2017/9/10 Peking University
@@ -82,6 +26,9 @@ import argparse
 #     os.makedirs(args.output_path)
 # except Exception:
 #     pass
+
+Path = path.join(path.split(path.split(path.abspath(path.dirname(__file__)))[0])[0], 'medical_data')
+
 
 
 def remove_bracket_from_str(str):
@@ -132,8 +79,8 @@ def remove_quotation_from_str(str):
 original_path = 'E:\medical data\MIMICIII_data'
 
 def get_revert_prescription():
-    prescription_df = pd.read_csv(os.path.join(original_path, 'PRESCRIPTIONS.csv'), dtype=str)
-    drug_df = CsvUtility.read_pickle('../data-repository/prescription_drug_over.pkl', 'r')
+    prescription_df = pd.read_csv(os.path.join(Path, 'MIMICIII_data/PRESCRIPTIONS.csv'), dtype=str)
+    drug_df = CsvUtility.read_pickle(Path+'/data-repository/prescription_drug_over.pkl', 'r')
     # print type(list(drug_df.index)[0])
     # print np.array(list(drug_df.index), dtype=str)
     print prescription_df.shape
@@ -194,12 +141,12 @@ def get_revert_prescription():
     print revert_prescrip_dict
     print len(revert_prescrip_dict)
 
-    CsvUtility.write_dict2csv(dict(revert_prescrip_dict), "../data-repository", 'revert_prescription_dict.csv')
+    CsvUtility.write_dict2csv(dict(revert_prescrip_dict), Path+"/data-repository", 'revert_prescription_dict.csv')
 
 # labtest revert dict, same to prescription
 def get_revert_labtest():
-    labtest_df = pd.read_csv(os.path.join(original_path, 'D_LABITEMS.csv'), dtype=str)
-    item_df = CsvUtility.read_pickle('../data-repository/lab_item_over.pkl', 'r')
+    labtest_df = pd.read_csv(os.path.join(Path, 'MIMICIII_data/D_LABITEMS.csv'), dtype=str)
+    item_df = CsvUtility.read_pickle(Path+'/data-repository/lab_item_over.pkl', 'r')
     print item_df[:5]
     print type(list(item_df.index)[0])
     print labtest_df.shape
@@ -228,7 +175,7 @@ def get_revert_labtest():
 
     print revert_labtest_dict
     print len(revert_labtest_dict)
-    CsvUtility.write_dict2csv(dict(revert_labtest_dict), "../data-repository", "revert_labtest_dict.csv")
+    CsvUtility.write_dict2csv(dict(revert_labtest_dict), Path+"/data-repository", "revert_labtest_dict.csv")
 
 # why limit the ICD code to 3, and that will make the number of ICD code to be 1000+, if we donnot limit the ICD code \
 # the number will be 5000+
@@ -244,10 +191,10 @@ def get_revert_diagnoses_procedures():
         , "", "-", "c", "c-c", "w", "e", "o", "b", "m", "g"
         , "s", "h", "t-t", "un", "ve", "k", "u", "j", "t"
         , "n"}
-    diagnoses_df = CsvUtility.read_pickle('../data-repository/icd_diagnoses_over.pkl', 'r')
-    procedures_df = CsvUtility.read_pickle('../data-repository/icd_procedures_over.pkl', 'r')
-    data_diagnoses = pd.read_csv(os.path.join(original_path, "D_ICD_DIAGNOSES.csv"), dtype=str)[["ICD9_CODE", "LONG_TITLE"]]
-    data_procedures = pd.read_csv(os.path.join(original_path, "D_ICD_PROCEDURES.csv"), dtype=str)[["ICD9_CODE", "LONG_TITLE"]]
+    diagnoses_df = CsvUtility.read_pickle(Path+'/data-repository/icd_diagnoses_over.pkl', 'r')
+    procedures_df = CsvUtility.read_pickle(Path+'/data-repository/icd_procedures_over.pkl', 'r')
+    data_diagnoses = pd.read_csv(os.path.join(Path, 'MIMICIII_data/D_ICD_DIAGNOSES.csv'), dtype=str)[["ICD9_CODE", "LONG_TITLE"]]
+    data_procedures = pd.read_csv(os.path.join(Path, 'MIMICIII_data/D_ICD_PROCEDURES.csv'), dtype=str)[["ICD9_CODE", "LONG_TITLE"]]
     data_diagnoses.set_index(["ICD9_CODE"], inplace=True)
     data_procedures.set_index(["ICD9_CODE"], inplace=True)
     print diagnoses_df[:5]
@@ -280,9 +227,9 @@ def get_revert_diagnoses_procedures():
         revert_diagnoses_procedures[" ".join(wordlist)] = icd_merge_list[i][0]
         for word in wordlist:
             word_count[word] = word_count[word] + 1 if word in word_count else 1
-    CsvUtility.write_dict2csv(revert_diagnoses_procedures, '../data-repository/', 'revert_diagnoses_procedures.csv')
+    CsvUtility.write_dict2csv(revert_diagnoses_procedures, Path+'/data-repository/', 'revert_diagnoses_procedures.csv')
     # CsvUtility.write_text2csv(word_count, '../data-repository/', 'revert_ICD_word_dict.csv')
-    with open("../data-repository/revert_ICD_word_dict.csv", 'w') as w:
+    with open(Path+"/data-repository/revert_ICD_word_dict.csv", 'w') as w:
             for (key, value) in sorted(word_count.items(), key=lambda s: s[1], reverse=True):
                 w.write(key + "," + str(value) + "\n")
 
@@ -292,7 +239,7 @@ def show_df(df, num=5):
 
 #test the method
 def get_final_word_dict():
-    MIMIC_word_dict = list(CsvUtility.read_pickle('../data-repository/event_instance_dict.pkl', 'r'))
+    MIMIC_word_dict = list(CsvUtility.read_pickle(Path+'/data-repository/event_instance_dict.pkl', 'r'))
     print MIMIC_word_dict[:10]
     print len(MIMIC_word_dict)
     diag_num = 0
@@ -317,9 +264,9 @@ def get_final_word_dict():
 
     print 'diagnoses number :', diag_num, 'labtest number:', lab_num,'drug number:', drug_num,'other number:', other_num
 
-    revert_diag_proce_df = pd.read_csv('../data-repository/revert_diagnoses_procedures.csv', header=None, dtype=str)
-    revert_labtest_df = pd.read_csv('../data-repository/revert_labtest_dict.csv', header=None, dtype=str)
-    revert_prescrip_df = pd.read_csv('../data-repository/revert_prescription_dict.csv', header=None, dtype=str)
+    revert_diag_proce_df = pd.read_csv(Path+'/data-repository/revert_diagnoses_procedures.csv', header=None, dtype=str)
+    revert_labtest_df = pd.read_csv(Path+'/data-repository/revert_labtest_dict.csv', header=None, dtype=str)
+    revert_prescrip_df = pd.read_csv(Path+'/data-repository/revert_prescription_dict.csv', header=None, dtype=str)
 
     show_df(revert_diag_proce_df, 10)
     show_df(revert_labtest_df, 10)
@@ -340,7 +287,7 @@ def get_final_word_dict():
 
     merge_df.drop_duplicates()
     show_df(merge_df)
-    merge_df.to_csv('../data-repository/entity_dict.csv', header=None, index=None)
+    merge_df.to_csv(Path+'/data-repository/entity_dict.csv', header=None, index=None)
     # here we get :
     # from mimic : diagnoses number : 3208 labtest number: 174 drug number: 749 other number: 220
     # from revert descriptions we get diagnoses number : 4356 labtest number: 174 drug number: 1434
